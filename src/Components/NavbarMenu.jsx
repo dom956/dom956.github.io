ee0a2329da5c5c66606cff3a80f42cd75a2723b5
osher volotker <volotkero@gmail.com>
2026-05-19 01:47:06 +0300

    - fixed issue with react scripts - modified press kit link

--- src/Components/NavbarMenu.jsx
+++ src/Components/NavbarMenu.jsx
@@ -22,7 +22,7 @@ function NavbarMenu() {
         <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
         <Nav.Link as={Link} to="/about">About</Nav.Link>
         <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-        <Nav.Link as={Link} to="https://drive.google.com/drive/u/0/folders/1lyrkLqIf8DC3n_sB8ps8GkxDY4R1dl6h" target="_blank" rel="noopener noreferrer">
+        <Nav.Link as={Link} to="https://drive.google.com/drive/folders/1THWGmM4M1S1JuLMUIWQjFy_l6N7C4W1u?usp=sharing" target="_blank" rel="noopener noreferrer">
         Press Kit
       </Nav.Link>
       </Nav>
